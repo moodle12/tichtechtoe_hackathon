28534c2f9a3df02df23b4f689f92a6f9f764d811
Mahmood Topiwala <103280407+moodle12@users.noreply.github.com>
2023-09-23 15:15:53 +0530

    chat feature commit

---
+++ alumni_chat/src/firebase.js
@@ -0,0 +1,22 @@
+// // Firebase deps
+// import firebase from 'firebase/app';
+// import 'firebase/auth';
+// import 'firebase/firestore';
+// // Hooks
+
+// // Components
+
+// firebase.initializeApp({
+//   apiKey: "AIzaSyDgDl23FvGcHLBpZQ8E-uQosdTIEqbHZgM",
+//   authDomain: "firechat-e885d.firebaseapp.com",
+//   projectId: "firechat-e885d",
+//   storageBucket: "firechat-e885d.appspot.com",
+//   messagingSenderId: "1090971171381",
+//   appId: "1:1090971171381:web:1fc2bfb59a6f19bb084f7c",
+//   measurementId: "G-BBJGN0ZSZ3"
+// });
+// const auth = firebase.auth();
+// const db = firebase.firestore();
+
+   
+// export {db,auth}
